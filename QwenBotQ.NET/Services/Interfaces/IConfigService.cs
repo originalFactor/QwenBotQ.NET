0bfb042202df1590a0d7c562566a8d556b0e1bcb
originalFactor <2438926613@qq.com>
2025-07-09 02:55:15 +0800

    rm deprecated

--- QwenBotQ.NET/Services/Interfaces/IConfigService.cs
+++
@@ -1,9 +0,0 @@
-namespace QwenBotQ.NET.Services.Interfaces;
-
-public interface IConfigService
-{
-    string OneBotServer { get; }
-    string? OneBotToken { get; }
-    string MongoUri { get; }
-    string MongoDbName { get; }
-}
--- QwenBotQ.NET/Services/Interfaces/IDatabaseService.cs
+++
@@ -1,8 +0,0 @@
-using QwenBotQ.NET.Models;
-
-namespace QwenBotQ.NET.Services.Interfaces;
-
-public interface IDatabaseService
-{
-    Task<UserModel?> GetUserAsync(string userId);
-}
